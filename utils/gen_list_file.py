cc24abeb72393ae37b9755792ccc27bf3a6630f0
YUAN Lingping <yuanlingping@YUANs-MacBook-Pro.local>
2019-01-15 11:33:26 +0800

    modify gen_list_file, can be used as start to generate new branch

--- utils/gen_list_file.py
+++ utils/gen_list_file.py
@@ -8,29 +8,29 @@ import time
 from scipy.misc import imread
 
 # train file
-data_dir = '../dataset/stackedBar_40/chart'
-label_dir = '../dataset/stackedBar_40/legend'
+import random
+
+data_dir = '../dataset/chart/'
+label_dir = '../dataset/legend/'
 
 im_paths = sorted(glob.glob(os.path.join(data_dir, '*.png')))
 gt_paths = sorted(glob.glob(os.path.join(label_dir, '*.png')))
 
-assert len(im_paths) == len(gt_paths)
+index_array = random.shuffle(range(len(im_paths)))
 
-train = open('../dataset/stackedBar_40/train.txt', 'w')
-for i in range(len(im_paths)):
-	print>>train, '{}\t{}'.format(im_paths[i],gt_paths[i])
+train = open('../dataset/train.txt', 'w')
+for i in range(300):
+	index = index_array[i] + 1
+	im_path = data_dir + "C" + str(index) + ".png"
+	gt_path = label_dir + "L" + str(index) + ".png"
+	print>>train, '{}\t{}'.format(im_path, gt_path)
 train.close()
 
-# test file
-test_data_dir = '../dataset/stackedBar_40/test_chart'
-test_label_dir = '../dataset/stackedBar_40/test_legend'
 
-im_paths = sorted(glob.glob(os.path.join(test_data_dir, '*.png')))
-gt_paths = sorted(glob.glob(os.path.join(test_label_dir, '*.png')))
-
-assert len(im_paths) == len(gt_paths)
-
-test = open('../dataset/stackedBar_40/test.txt', 'w')
-for i in range(len(im_paths)):
-	print>>test, '{}\t{}'.format(im_paths[i],gt_paths[i])
+test = open('../dataset/test.txt', 'w')
+for i in range(20):
+	index = index_array[i+300] + 1
+	im_path = data_dir + "C" + str(index) + ".png"
+	gt_path = label_dir + "L" + str(index) + ".png"
+	print>>test, '{}\t{}'.format(im_path, gt_path)
 test.close()
