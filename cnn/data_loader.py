fix bug on nan loss (normalization)

cnn/data_loader.py
@@ -51,7 +51,7 @@ def label_data_loader(start_index, batch_size, file_paths):
     labels = np.zeros((batch_size, label_height, label_width, label_channel), dtype=np.float32)
     for i in range(batch_size):
         file_path = file_paths[i + start_index]
-        lab = np.genfromtxt(file_path, delimiter=",")
+        lab = np.genfromtxt(file_path, delimiter=",") / 255.
         label = lab.reshape(label_height, label_width, label_channel)
         labels[i] = label
     return labels

cnn/infer.py
@@ -38,10 +38,12 @@ def main(_):
 		n = len(im_paths)
 		for i in xrange(n):
 			# im = imread(im_paths[i], mode='RGB') / 255.
-			im = np.genfromtxt(im_paths[i], delimiter=",")
-			gt = imread(gt_paths[i], mode='RGB') / 255.
+			im = imread(im_paths[i], mode='RGB') / 255.
+			gt = np.genfromtxt(gt_paths[i], delimiter=",") / 255.
 
 			pred = model.inference(im, sess)
+
+
 
 			plt.clf()
 			plt.subplot(211)